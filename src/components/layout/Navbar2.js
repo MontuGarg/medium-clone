e6069c7059ef67d79a5b49c83fad1e6fc91007f7
montu <montu0879.be20@chitkara.edu.in>
2025-03-06 12:47:15 +0530

    A

--- src/components/layout/Navbar2.js
+++ src/components/layout/Navbar2.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import image from "../../images/insights.jpg"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export default function  Navbar2() {
     const data=JSON.parse(localStorage.getItem("LoginUser"));
-
+    let navigate=useNavigate();
   return (
     <div id="navbar">
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -42,7 +42,7 @@ export default function  Navbar2() {
                     </li>
                     
                     <li className="nav-item">
-                    <Link className="nav-link" to="/" onClick={()=>localStorage.clear()}><img alt="" src='https://cdn-icons-png.flaticon.com/512/126/126467.png' style={{width:"30px",height:"30px"}}></img></Link>
+                    <button className="nav-link" onClick={()=>{localStorage.clear();navigate("/")}}><img alt="" src='https://cdn-icons-png.flaticon.com/512/126/126467.png' style={{width:"30px",height:"30px"}}></img></button>
                     </li>
                     <li className="nav-item">
                     <Link className="nav-link" to={`/profile/${data._id}`}><img alt="" src={data.image} style={{width:"35px",height:"35px",borderRadius:"100%",border:".2px solid black"}}></img></Link>
